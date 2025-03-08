3e7b708a6c7eed2068f309311689dea8c5dc6487
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-03-08 13:38:50 +0100

    adding heatmaps to create visual detections

--- detect_visual_cheat.py
+++ detect_visual_cheat.py
@@ -1,2 +1,12 @@
-import awpy.data
-import awpy.nav
+from awpy.plot import heatmap
+import polars as pl
+from awpy import Demo
+
+
+dem = Demo("demos/demo1.dem", verbose=False)
+
+
+player_locations = list(
+    dem.ticks.filter(pl.col("health") > 0, pl.col("side") == "ct")[["X", "Y", "Z"]].sample(100000).iter_rows()
+)
+fig, ax = heatmap(map_name="de_dust2", points=player_locations, method="hex", size=20)
